630597bd8335b8998708fedc38873ab8d6fe30d8
peterli-r3 <51169685+peterli-r3@users.noreply.github.com>
2024-01-02 19:35:54 +0800

    add css code

--- demo/app.py
+++ demo/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():  # put application's code here
-   return render_template("test2.html")
+   return render_template("index.html")
 
 if __name__ == '__main__':
    app.run(debug=True)
